4a31569255a2c933ea5b56e42990095665387d7f
Siarhei Drozd <siarhei_drozd@epam.com>
2023-04-04 20:11:28 +0300

    Task 3.2: add getProductById

--- products/handler.js
+++ products/handler.js
@@ -1,15 +1,26 @@
 'use strict';
 const products = require('./source/products.json');
+const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Credentials': true,
+  'content-type': 'application/json'
+}
 
-module.exports.getProductsList = async (event) => {
+exports.getProductsList = async () => {
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        product: await products
-      },
-      null,
-      2
-    ),
+    headers: corsHeaders,
+    body: JSON.stringify(await products)
+  };
+};
+
+exports.getProductsById = async (event) => {
+  const { productId } = event.pathParameters;
+  const product = await products.find(item => item.id === productId);
+
+  return {
+    statusCode: 200,
+    headers: corsHeaders,
+    body: JSON.stringify({product: product}),
   };
 };
