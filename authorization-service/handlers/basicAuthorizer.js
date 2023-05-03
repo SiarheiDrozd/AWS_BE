d675d5d886a9840d0d755b34d4a8333e96226e75
Siarhei Drozd <siarhei_drozd@epam.com>
2023-05-03 18:02:37 +0300

    Task 7: Add authorization

--- authorization-service/handlers/basicAuthorizer.js
+++ authorization-service/handlers/basicAuthorizer.js
@@ -1,4 +1,45 @@
+const generatePolicy = (effect, resource) => {
+  return {
+    Version: '2012-10-17',
+    Statement: [{
+      Action: 'execute-api:invoke',
+      Effect: effect,
+      Resource: resource
+    }]
+  };
+};
+
+const generateResponse = (effect, resource, principalId) => {
+  return {
+    principalId: principalId,
+    policyDocument: generatePolicy(effect, resource)
+  };
+};
 const basicAuthorizer = async (event) => {
+  console.log(event);
+
+  const { headers, methodArn } = event;
+  const principalId = 'test';
+
+  if (!headers.Authorization) {
+    console.log('Unauthorized request');
+
+    return {
+      statusCode: 401,
+      body: JSON.stringify({ message: 'You are not authorized to access this resource.' })
+    };
+  }
+
+  const { userName, password } = new Buffer.from(headers.Authorization.split(' ')[1],
+    'base64').toString().split(':');
+
+  const response = password === process.env[userName] ?
+    generateResponse('Allow', methodArn, principalId) :
+    generateResponse('Deny', methodArn, principalId);
+
+  console.log(JSON.stringify(response));
+
+  return response;
 };
 
 export default basicAuthorizer;
